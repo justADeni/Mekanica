551889629093b17a4cdac4f60ab1131945e899df
justADeni <deninikdo@gmail.com>
2022-06-10 23:31:32 +0200

    made InvManager

--- src/main/java/justadeni/mekanica/inventories/InvManager.java
+++ src/main/java/justadeni/mekanica/inventories/InvManager.java
@@ -1,4 +1,12 @@
 package justadeni.mekanica.inventories;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+
 public class InvManager {
+
+    private Inventory inventory;
+
+
+
 }
